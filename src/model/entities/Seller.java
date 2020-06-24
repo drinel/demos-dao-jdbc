6c54617f4b5fc497e5474d6c1cec7d8935d21e47
William Alex <will.i.am.002@hotmail.com>
2020-06-24 09:32:29 -0300

    FindById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -106,8 +106,8 @@ public class Seller implements Serializable{
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + ", department=" + department + "]";
+		return "Seller id:" + id + ", name:" + name + ", email:" + email + ", birthDate:" + birthDate + ", baseSalary:R$"
+				+ baseSalary + ", department:" + department;
 	}
 	
 	
