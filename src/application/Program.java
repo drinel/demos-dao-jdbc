a44bca465d72ab33925c9c41e2a10c260834e54b
William Alex <will.i.am.002@hotmail.com>
2020-06-30 07:55:03 -0300

    Update implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -46,7 +46,16 @@ public class Program {
 	        Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date() , 4000.0, department);
 	        sellerDao.insert(newSeller);
 	        System.out.println("Inserted! new id: " + newSeller.getId());
+	        
+	        
+	        
+	        System.out.println("\n=== TEST 5: Seller update ===");
+	        seller = sellerDao.findById(11);
+	        seller.setName("Martha Wayne");
+	        sellerDao.update(seller);
+	        System.out.println("Update completed");
 	    }
+	
 	}
 
 
